cd5ecf475a2f043533487005467758468c6d6ef2
Linck's Roberto Ripka Siqueira <lstyph@hotmail.com>
2017-11-20 21:50:59 -0200

    models

---
+++ Backend/Backend/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Web.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string Nome { get; set; }
+
+        public int DanoMaximo { get; set; }
+
+        public int Forca { get; set; }
+
+        public int Agilidade { get; set; }
+
+        public int Defesa { get; set; }
+
+
+        // Relacionamento item -- TipoItem
+        public int TipoItemID { get; set; }
+
+        public virtual TipoItem _TipoItem { get; set; }
+
+    }
+}
